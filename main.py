0539d7edbb20ec9e354c3e22a3e92ea7cb903fa9
K1A2 <jckim0307@gmail.com>
2024-01-22 22:01:56 +0900

    feat: argparse 로그 패스 지정 추가

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+import argparse
+
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--logger_path', type=str, help='실행 로그 저장 위치 지정')
+
+    args = parser.parse_args()
+
+    logger_path = ''
