9100506c6e51eb5b30ceac8a7bb9f5967cce5836
K1A2 <jckim0307@gmail.com>
2024-01-25 19:06:58 +0900

    feat: logger 초기화 함수 작성

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 import argparse
+import os
+
+from datetime import datetime
+
+import logger.default_logger
+from logger.default_logger import Logger
+
+
+def init_logger(args: argparse.Namespace,
+                logger_name: str,
+                logger_level: str,
+                logger_file_path: str,
+                logger_file_name: str) -> logger.default_logger.Logger:
+    logger = Logger(log_name=logger_name, level=logger_level)
+
+    if args.logger_print == 1:
+        logger.add_stream_handler()
+
+    # logger_file_name = datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + '.log'
+    # logger_file_path = './logs'
+
+    if not os.path.isdir(logger_file_path):
+        os.makedirs(logger_file_path)
+
+    logger_file_path = os.path.join(logger_file_path, logger_file_name)
+    logger.add_file_handler(logger_file_path)
+
+    return logger
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--logger_path', type=str, help='실행 로그 저장 위치 지정')
-
+    parser.add_argument('--logger_print', type=int, help='로그 콘솔 출력 여부 true는 1/false는 0', default=0)
     args = parser.parse_args()
-
-    logger_path = ''
