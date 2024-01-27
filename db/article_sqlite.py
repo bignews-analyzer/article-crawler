d4e7f7db8a480904baba56265cc3dcd7f6fdf4e1
K1A2 <jckim0307@gmail.com>
2024-01-27 14:29:12 +0900

    feat: 데이터베이스 생성 초기화 추가

--- db/article_sqlite.py
+++ db/article_sqlite.py
@@ -1,4 +1,5 @@
 import logging
+import typing
 
 from db.default_sqlite import SqliteHelper
 from db.static_sqlite import *
@@ -11,7 +12,17 @@ class ArticleSqliteHelper(SqliteHelper):
         super().__init__(logger, path)
         self._path = path
         self._logger.info(f'{self._path} database init')
+        self.__create_database()
 
-    def create_database(self):
+    def __create_database(self):
         self._create_database(SQL_ARTICLE_TABLE_CREATE)
         self._logger.debug(f'article table created')
+
+    def insert_values(self, values: typing.List[typing.Any]):
+        self._conn.executemany('INSERT INTO article VALUES (?,?,?,?)', values)
+        self._conn.commit()
+        self._logger.debug(f'{len(values)} values inserted')
+
+    def close(self):
+        super()._close_database()
+        self._logger.debug(f'{self._path} db closed')
