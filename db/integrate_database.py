9f09443630428d09ffb6820bf42211a8d23f4cb6
K1A2 <jckim0307@gmail.com>
2024-02-05 17:11:38 +0900

    feat: 데이터 통합

--- db/integrate_database.py
+++ db/integrate_database.py
@@ -4,14 +4,26 @@ import sqlite3
 
 from db.static_sqlite import *
 
-data_path = '../data/2024_01_28_01_03_28'
+data_path = '../data'
 datas = []
-for name in os.listdir(data_path):
-    conn = sqlite3.connect(os.path.join(data_path, name))
-    cur = conn.cursor()
-    result = cur.execute("SELECT * FROM article").fetchall()
-    datas += result
-    conn.close()
+for dir_name in os.listdir(data_path):
+    path = os.path.join(data_path, dir_name)
+    if os.path.isfile(path):
+        continue
+    for name in os.listdir(path):
+        conn = sqlite3.connect(os.path.join(data_path, dir_name, name))
+        cur = conn.cursor()
+        result = cur.execute("SELECT * FROM article").fetchall()
+        datas += result
+        conn.close()
+
+print(f'before total len: {len(datas)}')
+
+u_data = dict()
+for item in datas:
+    u_data[item[1] + item[2]] = item
+
+datas = list(u_data.values())
 
 for i in range(len(datas)):
     datas[i] = list(datas[i])
@@ -20,7 +32,7 @@ for i in range(len(datas)):
 random.shuffle(datas)
 print(f'total len: {len(datas)}')
 
-conn = sqlite3.connect(os.path.join('../data', '2024_01_28_01_03_28_integrate.db'))
+conn = sqlite3.connect(os.path.join('../data', 'integrate_remove.db'))
 cur = conn.cursor()
 cur.execute(SQL_ARTICLE_TABLE_CREATE)
 conn.commit()
